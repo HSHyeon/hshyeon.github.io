d35d9c3a1dd525ff4aaa47de17f912baa51e7e1e
[SW7] 홍소현 <hshyeon86@gmail.com>
2024-01-25 13:03:27 +0900

    초기 setting

---
+++ src/pages/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './AboutPage.css'
+
+function AboutPage() {
+  return (
+    <>
+    <div className='about'>
+    <div>FRONTEND-DEV</div>
+    <div>SOHYEON HONG</div></div>
+ </> 
+ )
+}
+
+export default AboutPage
---
+++ src/pages/works/WorksPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './WorksPage.css'
+
+function WorksPage() {
+  return (
+    <div className='works'>WorksPage</div>
+  )
+}
+
+export default WorksPage
