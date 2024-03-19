bdbd27f845b5d56926ab22932ed752853e08a277
[SW7] 홍소현 <hshyeon86@gmail.com>
2024-03-20 00:09:10 +0900

    [Feat] 푸터

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+function Footer() {
+  return <FooterContainer>Footer</FooterContainer>;
+}
+
+export default Footer;
+
+const FooterContainer = styled.footer`
+  background-color: gray;
+  height: 30vh;
+`;
