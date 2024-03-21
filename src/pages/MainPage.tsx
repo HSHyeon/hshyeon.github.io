56f0c3100a27477855de5af5f54e37adeb571998
[SW7] 홍소현 <hshyeon86@gmail.com>
2024-03-21 12:37:55 +0900

    error 발견

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,16 +1,22 @@
 import { SectionsContainer, Section } from "react-fullpage";
-
 import Header from "../components/Header";
-
 import Intro from "../components/Intro";
 import About from "../components/About";
 import Works from "../components/Works";
+//import { useState } from "react";
 
 const MainPage = () => {
+  //const [activeSection, setActiveSection] = useState(2);
+  //const handleSetActive = (to: string) => {
+  //setActiveSection(Number(to)); // 해당 섹션의 인덱스를 활성화된 섹션으로 설정
+  //TODO 새로고침했을 때 이상한 페이지로 튀는 에러 발견
+  //아무래도 몇번째 앵커인지를 인지하지 못해 나타나는 현상같음 수정하기
+  //};
   let options = {
     anchors: ["Intro", "About", "Works"],
-    navigation: false, // use dots navigatio
-    delay: 600, // the scroll animation speed
+    navigation: false,
+    delay: 600,
+    //activeSection: activeSection, // 현재 활성화된 섹션 설정
   };
 
   return (
