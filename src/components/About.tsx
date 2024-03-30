3cc69bca177b1920f6d90a727b0dfa00fc7c6283
[SW7] 홍소현 <hshyeon86@gmail.com>
2024-03-31 00:50:02 +0900

    feat about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <AboutCatainer>
       <TextContainer>
-        <h2>About</h2>
+        <p>다양한 경험을 통해 빠르게 적응하는 개발자 홍소현입니다</p>
       </TextContainer>
     </AboutCatainer>
   );
@@ -15,6 +15,10 @@ export default About;
 const AboutCatainer = styled.div`
   background-color: #b9a8c8;
   height: 100vh; /* 화면 높이의 90%로 설정 */
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  display: flex;
 `;
 
 const TextContainer = styled.div`
